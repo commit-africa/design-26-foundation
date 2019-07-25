0b1fa4d46f52ed4b9e7d35e2fd261d31273667ae
Shailen Naidoo <naidooshailen648@gmail.com>
2019-07-25 21:23:25 +0200

    Added banner component and pulling data from API

--- layouts/default.js
+++ layouts/default.js
@@ -7,6 +7,9 @@ module.exports = ({ title, content }) => html`
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="">
+    <script src="https://cdn.jsdelivr.net/npm/vue/dist/vue.js"></script>
+    <script src="https://unpkg.com/axios/dist/axios.min.js"></script>
     <title>${title}</title>
   </head>
   <body>
