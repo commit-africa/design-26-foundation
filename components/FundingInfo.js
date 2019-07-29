c383500d154f13c636b0478cff9d111dbbf5b30b
Shailen Naidoo <naidooshailen648@gmail.com>
2019-07-29 19:31:38 +0200

    Moved Funding Info into component

---
+++ components/FundingInfo.js
@@ -0,0 +1,20 @@
+const html = require('html-template-tag');
+
+const FundingInfo = () => html`
+  <section class="funding-info">
+    <article>
+      <div class="funding-info-block watch-video">
+        <img src="/public/assets/img/photos/sewing-four.jpg" alt="">
+      </div>
+      <div class="funding-info-block stats">
+        <h4>With your support</h4>
+        <p>We've trained <span class="stats-actual">30</span> young girls and can help many more.</p>
+        <a class="button" href="/about-us/">Read more about us</a>
+      </div>
+    </article>
+  </section>
+`;
+
+module.exports = {
+  FundingInfo,
+};
--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
 const html = require('html-template-tag');
 const axios = require('axios');
 const { AboutBlock } = require('../components/AboutBlock');
+const { FundingInfo } = require('../components/FundingInfo');
 
 const Banner = ({ image, heading, text, link, buttonText }) => html`
   <article>
@@ -22,20 +23,9 @@ const page = ({ banner }) => html`
       $${Banner(banner.right)}
     </section>
     ${banner.aboutBlocks.map(block => AboutBlock(block))}
-    <section class="funding-info">
-      <article>
-        <div class="funding-info-block watch-video">
-          <img src="/assets/img/photos/sewing-four.jpg" alt="">
-        </div>
-        <div class="funding-info-block stats">
-          <h4>With your support</h4>
-          <p>We've trained <span class="stats-actual">30</span> young girls and can help many more.</p>
-          <a class="button" href="/about">Read more about us</a>
-        </div>
-      </article>
-    </section>
+    $${FundingInfo()}
   </main>
-`;
+`; 
 
 function transformData (response) {
   return {
